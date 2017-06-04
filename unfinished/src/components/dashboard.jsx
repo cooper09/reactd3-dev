c5104ab048688d7254af8841a55b4372bb7a89e7
cooper smith <cooper@mpointinc.com>
2017-06-03 22:03:14 -0400

    layout dashboard skeleton

---
+++ unfinished/src/components/dashboard.jsx
@@ -0,0 +1,17 @@
+// unfinished/source/components/chart.jsx
+
+import React from 'react';
+
+export default (props) => {
+    return <h1>Hello, Dashboard</h1>;
+}
+
+export var DashboardApp = React.createClass ({
+    render: function() {
+        return 
+            <div>
+                "The Dasboard is Alive!!";
+            </div>
+    }//ed render function
+});
+
